2ae84750005fdb0ad53b86152341125dfd817d7b
SKALEZ-A <holumacbeth@gmail.com>
2024-06-30 21:37:05 +0100

    font

--- components/Collaboration.jsx
+++ components/Collaboration.jsx
@@ -11,9 +11,9 @@ import Image from "next/image";
 const Collaboration = () => {
   return (
     <Section crosses>
-      <div className="container lg:flex">
+      <div className="max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem] lg:flex">
         <div className="max-w-[25rem]">
-          <h2 className="h2 mb-4 md:mb-8">
+          <h2 className="text-[1.75rem] leading-[2.5rem] md:text-[2rem] md:leading-[2.5rem] lg:text-[2.5rem] lg:leading-[3.5rem] xl:text-[3rem] xl:leading-tight mb-4 md:mb-8">
             Pioneering Defi with Cross-chain innovations.
           </h2>
 
@@ -22,10 +22,10 @@ const Collaboration = () => {
               <li className="mb-3 py-3" key={item.id}>
                 <div className="flex items-center">
                   {/* <Image src={check} width={24} height={24} alt="check" /> */}
-                  <h6 className="body-2 ml-5">{item.title}</h6>
+                  <h6 className=" font-light text-[0.875rem] leading-6 md:text-base ml-5">{item.title}</h6>
                 </div>
                 {item.text && (
-                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
+                  <p className=" font-light text-[0.875rem] leading-6 md:text-base mt-3 text-n-4">{item.text}</p>
                 )}
               </li>
             ))}
@@ -35,14 +35,14 @@ const Collaboration = () => {
         </div>
 
         <div className="lg:ml-auto xl:w-[38rem] mt-4">
-          <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+          <p className=" font-light text-[0.875rem] leading-6 md:text-base mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
             {collabText}
           </p>
 
           <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
               <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
-                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                <div className="flex items-center justify-center w-full h-full bg-[#0E0C15] rounded-full">
                   <Image
                     src={logo}
                     width={58}
@@ -62,7 +62,7 @@ const Collaboration = () => {
                   }`}
                 >
                   <div
-                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
+                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-[#15131D] border border-n-1/15 rounded-xl -rotate-${
                       index * 45
                     }`}
                   >
